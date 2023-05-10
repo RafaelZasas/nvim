9c0669aba7be4600275a073baeccd4c90e85d021
Rafael Zasas <zasas.rafi@gmail.com>
2023-05-10 13:43:29 -0700

    Add package-lock to telescope ignore

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,12 +2,17 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
 require('telescope').setup({
     defaults = {
-        file_ignore_patterns = { 'node_modules', 'dist', 'build', 'out', 'venv', 'emulators-data' },
+        file_ignore_patterns = { 
+            'package-lock.json',
+            'node_modules', 'dist', 
+            'build', 'out', 
+            'venv', 'emulators-data'
+        },
         prompt_prefix = ' 🔍 ',
     },
     extensions = {

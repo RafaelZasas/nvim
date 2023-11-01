83c10e737ac72f9975d226f08f40fb1ea2af37f0
Rafael Zasas <zasas.rafi@gmail.com>
2023-10-31 19:03:26 -0700

    Fix LSP Zero config due to breaking changes

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,44 +1,57 @@
 local lsp = require('lsp-zero')
 
-lsp.preset("recommended")
-
-lsp.ensure_installed({
-    'tsserver',
-    'eslint',
-    'lua_ls',
-})
-
 -- Fix Undefined global 'vim'
-lsp.nvim_workspace()
+-- lsp.nvim_workspace()
 
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
+local cmp_format = require('lsp-zero').cmp_format()
 
 cmp.setup({
-    mapping = {
+    preselect = 'item',
+    completion = {
+        completeopt = 'menu,menuone,noinsert',
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+    },
+    mapping = cmp.mapping.preset.insert({
         -- `Enter` key to confirm completion
         ['<CR>'] = cmp.mapping.confirm({ select = false }),
 
         -- Ctrl+Space to trigger completion menu
         ['<C-Space>'] = cmp.mapping.complete(),
 
+        -- Abort completion
+        ['<C-e>'] = cmp.mapping.abort(),
+
         -- Navigate between snippet placeholder
         ['<C-f>'] = cmp_action.luasnip_jump_forward(),
         ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-    }
+
+        -- scroll up and down the documentation window
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+
+        -- SuperTab
+        ['<Tab>'] = cmp_action.luasnip_supertab(),
+        ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+    }),
+    -- Show source name in completion menu
+    formatting = cmp_format,
 })
-
-
 
 lsp.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
+    lsp.default_keymaps({ buffer = bufnr, preserve_mappings = false })
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>gn", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "<leader>gp", function() vim.diagnostic.goto_prev() end, opts)
     vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
@@ -51,6 +64,21 @@ end)
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+-- Setup Mason
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    -- Replace the language servers listed here
+    -- with the ones you want to install
+    ensure_installed = {
+        -- Web stuff
+        'tsserver', 'html', 'tailwindcss', 'svelte', 'eslint',
+        -- Must have language servers
+        'rust_analyzer', 'lua_ls', 'gopls', 'pylsp',
+    },
+    handlers = {
+        lsp.default_setup,
+    },
+})
 
 -- configure tsserver for neovim
 require('lspconfig').tsserver.setup({

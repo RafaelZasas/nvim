9fa2a1c8fa3b4f93baeb90660953d87c9d16dcd8
Rafael Zasas <zasas.rafi@gmail.com>
2024-02-25 10:12:59 +0200

    update colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -9,17 +9,17 @@ function ColorMyPencils(color)
             light = "latte",
             dark = "mocha",
         },
-        transparent_background = true, -- disables setting the background color.
-        show_end_of_buffer = true,     -- shows the '~' characters after the end of buffers
-        term_colors = false,           -- sets terminal colors (e.g. `g:terminal_color_0`)
+        transparent_background = false, -- disables setting the background color.
+        show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+        term_colors = true,             -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
-            enabled = false,           -- dims the background color of inactive window
+            enabled = true,             -- dims the background color of inactive window
             shade = "dark",
-            percentage = 0.15,         -- percentage of the shade to apply to the inactive window
+            percentage = 0.15,          -- percentage of the shade to apply to the inactive window
         },
-        no_italic = false,             -- Force no italic
-        no_bold = false,               -- Force no bold
-        no_underline = false,          -- Force no underline
+        no_italic = false,              -- Force no italic
+        no_bold = false,                -- Force no bold
+        no_underline = false,           -- Force no underline
         styles = {
             -- Handles the styles of general hi groups (see `:h highlight-args`):
             comments = { "italic", "bold" }, -- Change the style of comments
@@ -36,16 +36,47 @@ function ColorMyPencils(color)
             operators = {},
         },
         color_overrides = {},
-        custom_highlights = {},
+        custom_highlights = function(colors)
+            return {
+                --         Comment = { fg = colors.flamingo },
+                TabLineSel = { bg = colors.pink },
+                CmpBorder = { fg = colors.sky },
+                Pmenu = { bg = colors.overlay },
+                PmenuSbar = { bg = colors.overlay2 },
+                PmenuSel = { bg = colors.overlays },
+            }
+        end,
         integrations = {
             cmp = true,
             gitsigns = true,
             nvimtree = true,
             treesitter = true,
-            notify = false,
-            mini = false,
+            coc_nvim = true,
             mason = true,
             harpoon = true,
+            lsp_trouble = true,
+            telescope = {
+                enabled = true,
+                --style = "nvchad"
+            },
+            native_lsp = {
+                enabled = true,
+                virtual_text = {
+                    errors = { "italic" },
+                    hints = { "italic" },
+                    warnings = { "italic" },
+                    information = { "italic" },
+                },
+                underlines = {
+                    errors = { "underline" },
+                    hints = { "underline" },
+                    warnings = { "underline" },
+                    information = { "underline" },
+                },
+                inlay_hints = {
+                    background = true,
+                },
+            },
             -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
         },
     })

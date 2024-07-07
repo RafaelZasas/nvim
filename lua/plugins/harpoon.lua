ad5133a5e7f55a3a79b6410d7d270c0efbdc7654
zasas.rafi@gmail.com <zasas.rafi@gmail.com>
2024-07-06 21:28:48 -0700

    fix harpoon plugin for branch harpoon2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,26 +1,49 @@
-local harpoonConfig = function()
-  local mark = require 'harpoon.mark'
-  local ui = require 'harpoon.ui'
-
-  vim.keymap.set('n', '<leader>a', mark.add_file, { desc = 'Harpoon: Add file to menu' })
-  vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu, { desc = 'Harpoon: Toggle quick menu' })
-
-  vim.keymap.set('n', '<C-h>', function()
-    ui.nav_file(1)
-  end)
-  vim.keymap.set('n', '<C-j>', function()
-    ui.nav_file(2)
-  end)
-  vim.keymap.set('n', '<C-k>', function()
-    ui.nav_file(3)
-  end)
-  vim.keymap.set('n', '<C-l>', function()
-    ui.nav_file(4)
-  end)
-end
 return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  config = harpoonConfig(),
+  config = function()
+  local harpoon = require("harpoon")
+
+  vim.keymap.set(
+      'n', '<leader>a',
+      function() harpoon:list():add() end,
+      { desc = 'Harpoon: Add file to menu' }
+    )
+
+  vim.keymap.set('n', '<C-h>', function()
+    harpoon:list():select(1)
+  end)
+  vim.keymap.set('n', '<C-j>', function()
+    harpoon:list():select(2)
+  end)
+  vim.keymap.set('n', '<C-k>', function()
+    harpoon:list():select(3)
+  end)
+  vim.keymap.set('n', '<C-l>', function()
+    harpoon:list():select(4)
+  end)
+
+    local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+            results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+    }):find()
+end
+
+vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+    { desc = "Open harpoon window" })
+
+   -- End of config 
+end,
 }
